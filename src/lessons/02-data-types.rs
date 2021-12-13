0f408cddc579c30786ca0a9958c7a971062c3ce0
Lazar Nikolov <lazar.nikolov.94@gmail.com>
2021-12-13 06:35:44 +0100

    added arrays

--- src/lessons/02-data-types.rs
+++ src/lessons/02-data-types.rs
@@ -4,64 +4,136 @@
  */
 
 fn main() {
-  /*
-   * A scalar type represents a single value. There are four primary scalar types in Rust:
-   * - integers
-   * - floating-point numbers
-   * - booleans
-   * - characters
-   */
+    /*
+     * A scalar type represents a single value. There are four primary scalar types in Rust:
+     * - integers
+     * - floating-point numbers
+     * - booleans
+     * - characters
+     */
 
-  // ========= INTEGERS =========
-  /*
-   * An integer is a number without a fractional component. Integers come in six variants:
-   * - 8-bit
-   * - 16-bit
-   * - 32-bit
-   * - 64-bit
-   * - 128-bit
-   * - arch
-   *
-   * Each variant can be either Signed or Unsigned.
-   * Signed and Unsigned refer to whether it's possible for the number to be negative.
-   * Signed integers can be negative (they can have a sign, a "minus").
-   * Unsigned integers are always positive (we don't write +6, just 6).
-   */
-  let x: u32 = 5;
-  println!("x is an unsigned 32-bit integer with the value of {}", x);
+    // ========= INTEGERS =========
+    /*
+     * An integer is a number without a fractional component. Integers come in six variants:
+     * - 8-bit
+     * - 16-bit
+     * - 32-bit
+     * - 64-bit
+     * - 128-bit
+     * - arch
+     *
+     * Each variant can be either Signed or Unsigned.
+     * Signed and Unsigned refer to whether it's possible for the number to be negative.
+     * Signed integers can be negative (they can have a sign, a "minus").
+     * Unsigned integers are always positive (we don't write +6, just 6).
+     */
+    let x: u32 = 5;
+    println!("x is an unsigned 32-bit integer with the value of {}", x);
 
-  /*
-   * Rust is also capable of inferring the type of the value, but not always.
-   * In the case below, the type of y is i32, a default type that Rust assigns for integers.
-   */
-  let y = 15;
-  println!("Type type of y is i32, inferred by the value of {}", y);
+    /*
+     * Rust is also capable of inferring the type of the value, but not always.
+     * In the case below, the type of y is i32, a default type that Rust assigns for integers.
+     */
+    let y = 15;
+    println!("Type type of y is i32, inferred by the value of {}", y);
 
-  // ========= FLOATS =========
-  /*
-   * A floating-point number is a number with decimal points. We'll call them floats from here on.
-   * Rust have two primitive types for floats: 32-bits and 64-bits (f32 and f64).
-   * The 32-bit float is also called a single precision float.
-   * The 64-bit float is a double-precision float.
-   * The default is the 64-bit float because on modern CPUs it's rougly the same speed as the 32-bit one,
-   * but it's capable of more precision.
-   */
-  let pi: f32 = 3.14;
-  println!("The value of pi is: {}", pi);
+    // ========= FLOATS =========
+    /*
+     * A floating-point number is a number with decimal points. We'll call them floats from here on.
+     * Rust have two primitive types for floats: 32-bits and 64-bits (f32 and f64).
+     * The 32-bit float is also called a single precision float.
+     * The 64-bit float is a double-precision float.
+     * The default is the 64-bit float because on modern CPUs it's rougly the same speed as the 32-bit one,
+     * but it's capable of more precision.
+     */
+    let pi: f32 = 3.14;
+    println!("The value of pi is: {}", pi);
 
-  // ========= BOOLEANS =========
-  /*
-   * Just like other programming languages, a Boolean type in Rust has two possible values: true and false.
-   * Booleans are one byte in size.
-   * We can define booleans using the bool keyword, or directly setting a true/false value.
-   */
-  let is_registered = true;
-  let is_active: bool = false;
-  println!("Is the user registered: {}", is_registered);
-  println!("Is the user active: {}", is_active);
+    // ========= BOOLEANS =========
+    /*
+     * Just like other programming languages, a Boolean type in Rust has two possible values: true and false.
+     * Booleans are one byte in size.
+     * We can define booleans using the bool keyword, or directly setting a true/false value.
+     */
+    let is_registered = true;
+    let is_active: bool = false;
+    println!("Is the user registered: {}", is_registered);
+    println!("Is the user active: {}", is_active);
 
-  // ========= TUPLES =========
-  /*
-   * Coming soon...
-   */
+    // ========= TUPLES =========
+    /*
+     * The tuple in Rust is a general way of grouping together a number of values that can be of different types.
+     * They have a fixed length, so once declared they can't grow or shrink in size.
+     * Tuples are created by writing comma separated values inside parentheses.
+     * Optionally, you can specify the type of each element.
+     */
+    let tup = (5, 12.46, true);
+    let data: (u8, bool, char, f64) = (150, false, 'R', 13.85);
+
+    /*
+     * There are two ways to get individual values out of a tuple.
+     * We can use pattern matching to destructure it.
+     * This creates three separate variables and assigns the respected values from the tuple.
+     */
+    let (num, float, boolean) = tup;
+    println!("The number value of tup is: {}", num);
+    println!("The float value of tup is: {}", float);
+    println!("The boolean value of tup is: {}", boolean);
+
+    /*
+     * We can also access individual values by using a period, followed by the index of the value.
+     * Just like in other programming languages, the first index in a tuple is 0.
+     */
+    let first = data.0;
+    let second = data.1;
+    let third = data.2;
+    let fourth = data.3;
+    println!("The first value of data is: {}", first);
+    println!("The second value of data is: {}", second);
+    println!("The third value of data is: {}", third);
+    println!("The fourth value of data is: {}", fourth);
+
+    // ========= ARRAYS =========
+    /*
+     * Arrays are another way to hold multiple values, but unlike the tup, all emenets must be of the same type.
+     * Arrays in Rust also have a fixed length, just like the tuples.
+     * They are useful when ou want your data allocated on the stack rather than the heap,
+     * or when you want to ensure you always have a fixed number of elements.
+     */
+    let _nums = [1, 2, 3, 4, 5];
+    //  ^ Don't mind the underscore before nums. It's there to get rid of the "unused variable" compiler warning.
+
+    /*
+     * You can write an array's type by using square brackets, and include the type of each element, a semicolon,
+     * and then the number of elements it'll hold.
+     */
+    let _nums: [char; 4] = ['R', 'u', 's', 't'];
+
+    /*
+     * If we don't have all the values at first, we can resrve the length of the array
+     * and pre-populate it with some value.
+     * The example below will create an array nums with the values [3, 3, 3, 3, 3].
+     */
+    let nums = [3; 5];
+
+    /*
+     * Just like in th other programming languages, we can access the elements through their index.
+     */
+    println!("The second element in nums is: {}", nums[1]);
+
+    /*
+     * If we try to access an element with index larger than the size of the array we'll get an error.
+     * In the following case we'll get the error immediately, because Rust knows that we're trying to access
+     * the twentieth element of an array that only has 5 elements.
+     * Uncomment the following line to see the error.
+     */
+    // println!("The twentieth element in nums is: {}", nums[19]);
+
+    /*
+     * The same error can happen at runtime as well. For example, getting the element position from the user input.
+     * In that case, the program will compile, but when we ask for an element with larger position we'll get
+     * a runtime error. Rust will "panic", and the program will exit at the point of using the invalid value
+     * in the indexing operation.
+     * Make sure to handle those cases accordingly.
+     */
 }
